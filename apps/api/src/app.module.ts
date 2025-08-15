3913d117eb77e6e406df60efb0b680d997d70bc6
小鱼儿 <2214380963@qq.com>
2025-08-15 16:48:04 +0800

    feat: 添加语料库分类模块到应用模块导入

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,9 +6,10 @@ import { AuthModule } from './modules/auth/auth.module';
 import { LanguagesModule } from './modules/languages/languages.module';
 import { WordsModule } from './modules/words/words.module';
 import { SentencesModule } from './modules/sentences/sentences.module';
+import { CorpusCategoriesModule } from './modules/corpus-categories/corpus-categories.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, UserModule, AuthModule, LanguagesModule, WordsModule, SentencesModule],
+  imports: [ConfigModule, DatabaseModule, UserModule, AuthModule, LanguagesModule, CorpusCategoriesModule, WordsModule, SentencesModule],
   controllers: [],
   providers: [],
 })
